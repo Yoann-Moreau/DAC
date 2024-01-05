41df3b7c99f73591bc149343ff03f059fef4b9f7
Yoann Moreau <yoann.moreau@tutanota.com>
2024-01-05 22:59:36 +0100

    WIP dac init command

--- src/main/java/fr/ethilvan/dac/DAC.java
+++ src/main/java/fr/ethilvan/dac/DAC.java
@@ -1,18 +1,13 @@
 package fr.ethilvan.dac;
 
-import com.sk89q.worldedit.regions.Region;
 import fr.ethilvan.dac.commands.DacCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.HashMap;
-
 public final class DAC extends JavaPlugin {
-
-	public HashMap<String, Region> regions;
 
 
 	public DAC() {
-		this.regions = new HashMap<>();
+
 	}
 
 
--- src/main/java/fr/ethilvan/dac/commands/subcommands/InitCommand.java
+++ src/main/java/fr/ethilvan/dac/commands/subcommands/InitCommand.java
@@ -1,10 +1,24 @@
 package fr.ethilvan.dac.commands.subcommands;
 
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.util.Location;
+import com.sk89q.worldedit.world.World;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
+import com.sk89q.worldguard.protection.regions.RegionQuery;
 import fr.ethilvan.dac.DAC;
 import fr.ethilvan.dac.commands.Subcommand;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+
+import java.util.Set;
 
 public class InitCommand extends Subcommand {
 	@Override
@@ -32,8 +46,55 @@ public class InitCommand extends Subcommand {
 			return;
 		}
 
-		commandSender.sendMessage(
-				Component.text("A DAC game has been created at your location.", NamedTextColor.GREEN)
-		);
+		Player player = Bukkit.getPlayer(commandSender.getName());
+		if (player == null) {
+			Bukkit.getLogger().warning("This command must be used by a player.");
+			return;
+		}
+
+		ConfigurationSection config = dac.getConfig().getConfigurationSection("regions");
+		if (config == null) {
+			player.sendMessage(Component.text("There are no defined DAC regions.", NamedTextColor.RED));
+			return;
+		}
+
+		Location wgLocation = BukkitAdapter.adapt(player.getLocation());
+		World wgWorld = BukkitAdapter.adapt(player.getWorld());
+
+		RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
+		RegionManager regionsManager = container.get(wgWorld);
+
+		if (regionsManager == null) {
+			player.sendMessage(Component.text("Error when retrieving world regions.", NamedTextColor.RED));
+			return;
+		}
+
+		Set<String> dacNames = config.getKeys(false);
+		for (String dacName : dacNames) {
+			String regionName = (String) config.get(dacName + ".base");
+
+			if (regionName == null) { // Skip iteration if the base region is not defined
+				continue;
+			}
+
+			ProtectedRegion region = regionsManager.getRegion(regionName);
+			RegionQuery query = container.createQuery();
+			ApplicableRegionSet set = query.getApplicableRegions(wgLocation);
+
+			for (ProtectedRegion item : set) {
+				if (item.equals(region)) {
+					createDacGame(player, dacName);
+					return;
+				}
+			}
+		}
+
+		player.sendMessage(Component.text("You are not in a DAC region.", NamedTextColor.RED));
+	}
+
+
+	private void createDacGame(Player player, String dacName) {
+		player.sendMessage(Component.text("A DAC game has been created in the " + dacName + " region.",
+				NamedTextColor.GREEN));
 	}
 }
